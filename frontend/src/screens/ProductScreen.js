ca2e960f40a6438efc72e2475033ca5ddd7bf9dd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-12-24 16:22:49 +0100

    WIP: finish product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find(product => product._id === match.params.id)
   return (
     <div>
-      Product
+      {product.name}
     </div>
   )
 }
