38dd3d542e0457f53f40936da466efff397c02d7
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-12-26 12:39:51 +0100

    Add users.js containing 3 users

---
+++ backend/data/users.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  { name: 'Admin User', email: 'admin@gmail.com', password: bcrypt.hashSync('123456', 10), isAdmin: true },
+  { name: 'George Bernard', email: 'george@gmail.com', password: bcrypt.hashSync('123456', 10) },
+  { name: 'John Hammer', email: 'john@gmail.com', password: bcrypt.hashSync('123456', 10) },
+]
+
+export default users
