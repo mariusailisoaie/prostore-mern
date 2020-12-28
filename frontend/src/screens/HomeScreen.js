33a7a8b1cc80a609f005bdad29e35974a93b354f
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-12-28 16:31:06 +0100

    Add loading spinner and message component for nice alerts

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { fetchProductsStartAsync } from '../actions/productActions'
 
 const HomeScreen = () => {
@@ -17,15 +19,17 @@ const HomeScreen = () => {
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {
-          products.map(product => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-              <Product product={product} />
-            </Col>
-          ))
-        }
-      </Row>
+      {
+        isFetching ? <Loader /> : errorMessage ? <Message variant='danger'>{errorMessage}</Message> : <Row>
+          {
+            products.map(product => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
+                <Product product={product} />
+              </Col>
+            ))
+          }
+        </Row>
+      }
     </div>
   )
 }
