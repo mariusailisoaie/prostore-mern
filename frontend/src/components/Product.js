214165731cfac0a4f35c298f318d16eb8a7bc9e4
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-12-28 16:08:48 +0100

    Format file

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,19 +6,19 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <Link to={`/product/${product._id}`}>
+      <Link to={`/product/${ product._id }`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${product._id}`}>
+        <Link to={`/product/${ product._id }`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
         <Card.Text as='div'>
-          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+          <Rating value={product.rating} text={`${ product.numReviews } reviews`} />
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
