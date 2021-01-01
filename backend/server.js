b173da488e00c3a40aa8e4d4b77b7e0f886b6350
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-01 16:17:00 +0100

    Add functionality for authenticating users with email and password

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,21 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Api is running.')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/user', userRoutes)
 
 app.use(notFound)
 
@@ -24,4 +27,4 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server running in ${ process.env.NODE_ENV } on port ${ PORT }`.yellow.bold))
