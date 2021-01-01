6c285eaf0e089d763e0b670d47f58a5b5dead0ac
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-01 20:47:22 +0100

    Create and protect getUserProfile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { authUser } from '../controllers/userController.js'
+import protect from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
