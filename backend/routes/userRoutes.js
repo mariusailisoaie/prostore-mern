d6cbca0a5a6b32e650f7bb6a3e06f185ac4f73a7
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-04 13:30:26 +0100

    Add BE functionality for updating user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import protect from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, signUpUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, signUpUser, updateUserProfile } from '../controllers/userController.js'
 
 router.route('/signup').post(signUpUser)
 router.post('/signin', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
