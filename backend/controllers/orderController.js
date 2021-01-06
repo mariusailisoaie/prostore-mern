0467a784b39a7f8b91150c75414d6a1dfa49c8e2
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-06 12:57:40 +0100

    Add BE functionality for getting an order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,18 @@ const createOrder = asyncHandler(async (req, res) => {
   }
 })
 
-export { createOrder }
+// @desc    Get order by ID
+// @route   GET /api/orders
+// @access  Protected
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { createOrder, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
 import protect from '../middleware/authMiddleware.js'
-import { createOrder } from '../controllers/orderController.js'
+import { createOrder, getOrderById } from '../controllers/orderController.js'
 
 router.route('/').post(protect, createOrder)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
