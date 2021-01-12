8d6d2de2fc45138cf60680f0fa6cd45e8118f4c1
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-12 16:35:44 +0100

    Add morgan for logging requests

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
@@ -14,6 +15,7 @@ connectDB()
 
 const app = express()
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('Api is running.')
