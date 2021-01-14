b86aed89032574fc5a42b4a5e2fa83a36bb258fd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-14 21:24:14 +0100

    Put HomeScreen route first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Switch>
+              <Route path='/' component={HomeScreen} exact />
               <Route path='/signin' component={SignInScreen} />
               <Route path='/signup' component={SignUpScreen} />
               <Route path='/profile' component={ProfileScreen} />
@@ -33,7 +34,6 @@ const App = () => {
               <Route path='/payment' component={PaymentMethodScreen} />
               <Route path='/placeorder' component={PlaceOrderScreen} />
               <Route path='/order/:id' component={OrderScreen} />
-              <Route path='/' component={HomeScreen} exact />
               <Route component={PageNotFoundScreen} />
             </Switch>
           </Container>
