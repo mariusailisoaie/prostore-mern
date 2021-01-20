e0275e138c63aed32d2f320261316794f27ac24f
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-20 15:10:03 +0100

    Restructure server

--- backend/server.js
+++ backend/server.js
@@ -20,24 +20,23 @@ const app = express()
 app.use(express.json())
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')))
-
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
-  })
-}
-
 app.use('/api/products', productRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 app.use(notFound)
 
